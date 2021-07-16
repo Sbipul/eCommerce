b4274e8cb22b4607671bec27a7ae3c93ed2176aa
Bipul-Ch-Roy <kumarroy.kb.kb@gmail.com>
2021-07-16 12:34:07 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	$(".bar span").click(function(){
+		$(".off").toggleClass("on-off");
+	});
+	$(".cross span").click(function(){
+		$(".off").removeClass("on-off");
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
